49b0ac2d1a70cf2a49f78c0ae3126a3dc5328862
ThiagoDev25 <thiagoafonsopaz@hotmail.com>
2024-10-20 02:05:18 -0300

    projectcard configurado

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,75 @@
 export const Projects = () => {
-    return (() => {
-        
-    })
+    
+    const projects = [
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+          },
+          {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
+          },
+          {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
+          },
+          {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+          },
+          {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
+          },
+          {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
+          },
+    ]
+    return (
+        <section className="project" id="project">
+            <Container>
+                <Row>
+                    <Col>
+                    <h2>Projetos</h2>
+                    <p>Este é um texto teste para ver o que irei falar aqui para convencer as pessoas a comprarem meu serviço!</p>
+                    <Tab.Container id="project-tabs" defaultActiveKey="first">
+                    <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content>
+                    <Tab.Pane eventKey="first">
+                        <Row>
+                            {
+                                projects.map((project, index) => {
+                                    return (
+                                        <p>{project.title}</p>
+                                    )
+                                })
+                            }
+                        </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">Lorem Ipsum</Tab.Pane>
+                    <Tab.Pane eventKey="third">Lorem ipsum</Tab.Pane>
+                  </Tab.Content>
+                  </Tab.Container>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
 }
